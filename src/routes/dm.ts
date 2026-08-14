387cad99cd929965ca858e3e338e8583c26e1964
nnachonesa <imfigliuolo@gmail.com>
2026-08-14 20:27:30 -0300

    refactor(dm):remove slash of url because broke ky prefixurl

--- src/routes/dm.ts
+++ src/routes/dm.ts
@@ -1,30 +1,25 @@
-import { type HttpClient } from '../http';
-import { type DmQuery, type DmReponse, type DmMessagesResponse } from '../types';
+import type { HttpClient } from '../http';
+import type { DmMessagesResponse, DmQuery, DmReponse } from '../types';
 
 export class DmAPI {
-    constructor (private readonly http: HttpClient) { }
-    /**
-     * GET /dms — Query: page_id — List page DMs
-     * GET /dms/{conversationId}/messages — Query: page_id — DM messages
-     *
-     */
-    list (query: DmQuery) {
-        return this.http.ky
-            .get('/dms', {
-                searchParams: {
-                    page_id: query.page_id
-                }
-            })
-            .json<DmReponse>();
-    }
+	constructor(private readonly http: HttpClient) {}
+	list(query: DmQuery) {
+		return this.http.ky
+			.get('dms', {
+				searchParams: {
+					page_id: query.page_id,
+				},
+			})
+			.json<DmReponse>();
+	}
 
-    getMessagesFromDM (query: DmQuery & { conversationId: number }) {
-        return this.http.ky
-            .get(`/dms/${ query.conversationId }/messages`, {
-                searchParams: {
-                    page_id: query.page_id
-                }
-            })
-            .json<DmMessagesResponse>();
-    }
+	getMessagesFromDM(query: DmQuery & { conversationId: number }) {
+		return this.http.ky
+			.get(`dms/${query.conversationId}/messages`, {
+				searchParams: {
+					page_id: query.page_id,
+				},
+			})
+			.json<DmMessagesResponse>();
+	}
 }
