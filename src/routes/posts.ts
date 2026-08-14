2b4b81da7a98c51258c1ab56e8c5f75690e224a9
nnachonesa <imfigliuolo@gmail.com>
2026-08-14 20:29:44 -0300

    fix(posts): change to the correct type

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,65 +1,65 @@
-import { type HttpClient } from '../http';
-import {
-    type CreatePostQuery,
-    type CreatePostResponse,
-    type DeletePostQuery,
-    type EditPostQuery,
-    type GetPostQuery,
-    type ListPostQuery,
-    type Posts,
-    type Post
+import type { HttpClient } from '../http';
+import type {
+	CreatePostQuery,
+	CreatePostResponse,
+	DeletePostQuery,
+	EditPostQuery,
+	GetPostQuery,
+	ListPostQuery,
+	Post,
+	Posts,
 } from '../types';
 
 export class PostsAPI {
-    constructor (private readonly http: HttpClient) { }
+	constructor(private readonly http: HttpClient) {}
 
-    list (query: ListPostQuery) {
-        return this.http.ky
-            .get('posts', {
-                searchParams: {
-                    page_id: query.pageId,
-                    cursor: query.cursor
-                }
-            })
-            .json<Posts>();
-    }
+	list(query: ListPostQuery) {
+		return this.http.ky
+			.get('posts', {
+				searchParams: {
+					page_id: query.pageId,
+					cursor: query.cursor,
+				},
+			})
+			.json<Posts>();
+	}
 
-    create (body: CreatePostQuery) {
-        return this.http.ky
-            .post('posts', {
-                json: body
-            })
-            .json<CreatePostResponse>();
-    }
+	create(body: CreatePostQuery) {
+		return this.http.ky
+			.post('posts', {
+				json: body,
+			})
+			.json<CreatePostResponse>();
+	}
 
-    get (query: GetPostQuery) {
-        return this.http.ky
-            .get(`posts/${ query.postId }`, {
-                searchParams: {
-                    page_id: query.pageId
-                }
-            })
-            .json<CreatePostResponse>();
-    }
+	get(query: GetPostQuery) {
+		return this.http.ky
+			.get(`posts/${query.postId}`, {
+				searchParams: {
+					page_id: query.pageId,
+				},
+			})
+			.json<Post>();
+	}
 
-    delete (query: DeletePostQuery) {
-        return this.http.ky.delete(`posts/${ query.postId }`, {
-            searchParams: {
-                page_id: query.page_id
-            }
-        });
-    }
+	delete(query: DeletePostQuery) {
+		return this.http.ky.delete(`posts/${query.postId}`, {
+			searchParams: {
+				page_id: query.page_id,
+			},
+		});
+	}
 
-    edit (query: EditPostQuery) {
-        return this.http.ky
-            .put(`posts/${ query.postId }`, {
-                searchParams: {
-                    page_id: query.page_id
-                },
-                json: {
-                    content: query.content
-                }
-            })
-            .json<Post>();
-    }
+	edit(query: EditPostQuery) {
+		return this.http.ky
+			.put(`posts/${query.postId}`, {
+				searchParams: {
+					page_id: query.page_id,
+				},
+				json: {
+					content: query.content,
+				},
+			})
+			.json<Post>();
+	}
 }
