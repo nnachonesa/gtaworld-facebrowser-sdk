297323a0d52a2f12143e17c706b7220caaefa853
nnachonesa <imfigliuolo@gmail.com>
2026-08-14 20:31:00 -0300

    fix(dm):change next_cursor/prev_cursor from boolean to string | null

--- src/types/dm.ts
+++ src/types/dm.ts
@@ -4,56 +4,58 @@
  *
  */
 export interface DmQuery {
-    page_id: number
+	page_id: number;
 }
 
 export interface DmReponse {
-    conversations: DmConversations[]
-    meta: DmMeta
+	conversations: DmConversations[];
+	meta: DmMeta;
 }
 
 interface DmConversations {
-    id: number
-    profile: DmProfile
-    last_message: DmLastMessage
-    last_message_at: string
+	id: number;
+	profile: DmProfile;
+	last_message: DmLastMessage;
+	last_message_at: string;
 }
 
 interface DmLastMessage {
-    id: number
-    content: string
-    from_page: boolean
-    created_at: string
+	id: number;
+	content: string;
+	from_page: boolean;
+	created_at: string;
 }
 
 interface DmProfile {
-    id: number
-    username: string
-    character_name: string
-    avatar_url: string
+	id: number;
+	username: string;
+	character_name: string;
+	avatar_url: string;
 }
 
 interface DmMeta {
-    current_page: number
-    last_page: number
-    per_page: number
-    total: number
+	current_page: number;
+	last_page: number;
+	per_page: number;
+	total: number;
 }
 // ---------------
+export interface DmMessage {
+	id: number;
+	content: string;
+	from_page: boolean;
+	sender: {
+		type: string;
+		page: {
+			id: number;
+			name: string;
+		};
+	};
+	created_at: string;
+}
+
 export interface DmMessagesResponse {
-    messages: [{
-        id: number
-        content: string
-        from_page: boolean
-        sender: {
-            type: string
-            page: {
-                id: number
-                name: string
-            }
-        }
-        created_at: string
-    }]
-    next_cursor: boolean
-    prev_cursor: boolean
+	messages: DmMessage[];
+	next_cursor: string | null;
+	prev_cursor: string | null;
 }
